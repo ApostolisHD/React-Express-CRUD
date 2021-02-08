a9fae757d6c0e999fc75a7f1061c053dc0e8abe9
ApostolisHD <tolis1943@windowslive.com>
2021-02-08 17:04:30 +0200

    tha ta katafer32o

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require ("express");
 const pool = require("./db");
 const app = express();
 const cors = require("cors");
+const path = require('path');
 
 
 var bodyParser = require('body-parser');
@@ -18,12 +19,13 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get("/")
+
 app.get("/employee", pool.getEmployee)
 app.post("/register", pool.createEmployee)
 app.put("/employee/:id", pool.updateEmployee)
 app.delete("/employee/:id", pool.deleteEmployee)
-//app.post("/employee/login", pool.loginEmployee)
-app.get("/employee/:id",pool.getEmployeeById)
-// app.post('/employee/login', pool.loginEmployee)
+
+
 
 app.listen (process.env.PORT || 5000 ,() => console.log("server running on port: http://localhost:5000"))
