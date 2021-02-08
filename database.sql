f8ca8c7842e757e1f7b1479ee2579a5226def84a
ApostolisHD <tolis1943@windowslive.com>
2021-02-08 23:38:11 +0200

    tha ta katafero pou tha paei

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
--- CREATE DATABASE employee_databse;
+CREATE DATABASE employee_databse;
 
--- -- \c into employee_databse
+-- \c into employee_databse
 
--- CREATE TABLE employee(
---     id   BIGINT PRIMARY KEY,
--- 	last_name  VARCHAR(50) NOT NULL,
--- 	first_name   VARCHAR(50) NOT NULL,
--- 	is_active BOOLEAN NOT NULL,
--- 	date_of_birth DATE NOT NULL	
--- );
+CREATE TABLE employee(
+    id   BIGINT PRIMARY KEY,
+	last_name  VARCHAR(50) NOT NULL,
+	first_name   VARCHAR(50) NOT NULL,
+	is_active BOOLEAN NOT NULL,
+	date_of_birth DATE NOT NULL	
+);
 
