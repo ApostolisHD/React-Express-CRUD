bb9dd39f02bbdc1fc30ad07d8d27e153eac8df1c
ApostolisHD <tolis1943@windowslive.com>
2021-02-10 14:16:15 +0200

    final

--- server/Routes.js
+++ server/Routes.js
@@ -1,9 +1,9 @@
 const { request, response } = require("express");
-const { Pool, Client } = require("pg");
+const { Pool} = require("pg");
 const bigInt = require("big-integer");
 
 
-const client = new Client({
+const pool = new Pool({
   user: "postgres",
   password: "123456",
   database: "employee_databse",
@@ -11,14 +11,14 @@ const client = new Client({
   port: 5432,
 })
 
-client.connect();
+
 
 //routes//
 
 //get all employees 
 const getEmployee = async (request, response) => {
   try {
-    await client.query("SELECT id, last_name, first_name, to_char(date_of_birth, 'DD/MM/YYYY') AS date_of_birth, is_active FROM employee ORDER BY id ASC ", (error, result) => {
+    await pool.query("SELECT id, last_name, first_name, to_char(date_of_birth, 'DD/MM/YYYY') AS date_of_birth, is_active FROM employee ORDER BY id ASC ", (error, result) => {
       response.status(200).json(result.rows)
     })
   } catch (error) {
@@ -30,7 +30,7 @@ const getEmployee = async (request, response) => {
 const getEmployeeById = async (request, response) => {
   const id = parseInt(request.params.id)
   try {
-    await client.query("SELECT id, last_name, first_name ,is_active FROM employee WHERE id=$1", [id], (error, result) => {
+    await pool.query("SELECT id, last_name, first_name ,is_active FROM employee WHERE id=$1", [id], (error, result) => {
       response.status(200).json(result)
     })
   } catch (error) {
@@ -42,7 +42,7 @@ const getEmployeeById = async (request, response) => {
 const createEmployee = async (request, response) => {
   const { last_name, first_name, date_of_birth, is_active } = request.body;
   try {
-    await client.query("INSERT INTO employee (last_name , first_name , date_of_birth, is_active) VALUES ($1, $2 ,$3,$4)",
+    await pool.query("INSERT INTO employee (last_name , first_name , date_of_birth, is_active) VALUES ($1, $2 ,$3,$4)",
       [last_name, first_name, date_of_birth, is_active]);
     response.status(201).send("Employee added!!!")
   } catch (error) {
@@ -54,7 +54,7 @@ const updateEmployee = async (request, response) => {
   const id = request.params.id
   const { last_name, first_name, is_active } = request.body;
   try {
-    client.query(
+    pool.query(
       "UPDATE employee SET last_name= $1, first_name= $2, is_active=$3 WHERE id = $4",
       [last_name, first_name, is_active, id])
     response.status(200).send("employee modified")
@@ -66,7 +66,7 @@ const updateEmployee = async (request, response) => {
 const deleteEmployee = async (request, response) => {
   const id = request.params.id
   try {
-    client.query("DELETE FROM employee WHERE id = $1", [id])
+    pool.query("DELETE FROM employee WHERE id = $1", [id])
     response.status(200).send(`employee deleted with ID: ${id}`)
   } catch (err) {
     console.error(error.message)
